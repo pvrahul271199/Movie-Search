1a6cacf897458827c6f0a741adc84d2bc653bb8d
Rahul <pvrahul.271199@gmail.com>
2021-05-08 16:26:44 +0530

    commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path')
+const axios = require('axios')
+const fetch = require('node-fetch');
+const app = express();
+const port = 8080;
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) => {
+
+    const movieName = req.query.movie;
+    const url = "http://api.tvmaze.com/search/shows/?q=" + movieName;
+    const response = await axios(url);
+    const json = await response.data;
+    console.log(typeof (json))
+    res.render('home', { json })
+
+})
+
+app.get('*', (req, res) => {
+    res.send("Page not Found")
+})
+
+
+app.listen(port, () => {
+    console.log(`Server listening in PORT ${port}`)
+})
